a59e0b219a9f5653c42f99ed1ca5f529086d9ff5
dni ⚡ <office@dnilabs.com>
2025-10-22 15:04:42 +0200

    chore: clean dead code (#2)
    * chore: clean dead code
    
    * fixup!
    
    * fixup!

--- __init__.py
+++ __init__.py
@@ -1,11 +1,6 @@
-import asyncio
-
 from fastapi import APIRouter
-from lnbits.tasks import create_permanent_unique_task
-from loguru import logger
 
 from .crud import db
-from .tasks import wait_for_paid_invoices
 from .views import scrum_generic_router
 from .views_api import scrum_api_router
 
@@ -21,26 +16,8 @@ scrum_static_files = [
     }
 ]
 
-scheduled_tasks: list[asyncio.Task] = []
-
-
-def scrum_stop():
-    for task in scheduled_tasks:
-        try:
-            task.cancel()
-        except Exception as ex:
-            logger.warning(ex)
-
-
-def scrum_start():
-    task = create_permanent_unique_task("ext_scrum", wait_for_paid_invoices)
-    scheduled_tasks.append(task)
-
-
 __all__ = [
     "db",
     "scrum_ext",
-    "scrum_start",
     "scrum_static_files",
-    "scrum_stop",
 ]

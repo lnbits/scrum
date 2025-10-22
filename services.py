7fda7d35692de39ab58a8f0c1d156c33b25e1b56
dni ⚡ <office@dnilabs.com>
2025-10-22 14:57:29 +0200

    chore: clean dead code

--- services.py
+++
@@ -1,7 +0,0 @@
-from lnbits.core.models import Payment
-from loguru import logger
-
-
-async def payment_received_for_tasks(payment: Payment) -> bool:
-    logger.info("Payment receive logic generation is disabled.")
-    return True
